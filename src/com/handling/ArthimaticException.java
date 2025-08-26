9c4d95e9df1a2bfc345e28f913d9d433c1b06ece
Jayasri8008 <jayasripeddiboyina3@gmail.com>
2025-08-26 21:35:00 +0530

    Update ArthimaticException.java

--- src/com/handling/ArthimaticException.java
+++ src/com/handling/ArthimaticException.java
@@ -1,4 +1,17 @@
 package com.handling;
 
 public class ArthimaticException {
+    public static void main(String[] args) {
+        int a=10;
+        int b=0;
+        try {
+           int  result = a / b;
+            System.out.println(result);
+        }catch (ArithmeticException ae){
+            System.out.println("don't divide with zero"+ ae.getMessage());
+        }
+        finally{
+            System.out.println("finally always printing");
+        }
+    }
 }
