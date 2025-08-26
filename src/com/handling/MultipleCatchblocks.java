937f96fc1d11a6e5403f101dc6999336f773c9a8
Jayasri8008 <jayasripeddiboyina3@gmail.com>
2025-08-26 21:33:26 +0530

    Update MultipleCatchblocks.java

--- src/com/handling/MultipleCatchblocks.java
+++ src/com/handling/MultipleCatchblocks.java
@@ -1,4 +1,19 @@
 package com.handling;
 
 public class MultipleCatchblocks {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4};
+        try {
+
+            System.out.println(arr[5]);
+
+        } catch (ArithmeticException ae) {
+            System.out.println("don't divide by 0" +" "+ ae.getMessage());
+        }
+        catch(RuntimeException r){
+            System.out.println("Runtime exception nothing but unchecked exceptions"+" "+r.getMessage());
+        }catch (Exception e){
+            System.out.println("Exception parent throwable"+" "+e.getMessage());
+        }
+    }
 }
